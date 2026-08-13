07bc32f9585683152f0b83c98256dff64dd6518b
Anshuman56 <anshumandehury@yandex.com>
2026-08-13 09:17:09 +0530

    add the schema of the user

--- projects/auth-basics-v2/server.js
+++ projects/auth-basics-v2/server.js
@@ -1,6 +1,27 @@
+require("dotenv/config");
 const bcrypt = require("bcryptjs");
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const { use } = require("react");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
 async function main() {
   try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("conneced");
+    const userSchema = await mongoose.Schema({
+      email: { type: String, require: true, uniqe: true },
+      passwordHash: { type: String, require: true },
+      createdAt: { type: Date, default: Date.now },
+    });
+
+    const User = await mongoose.model("User", userSchema);
     const hash = await bcrypt.hash("hello", 10);
     console.log(hash);
     const compare = await bcrypt.compare("helo", hash);
