c0893a772f6f7dcc4597270dfe54d33d3062dd4f
Anshuman56 <anshumandehury@yandex.com>
2026-08-13 10:48:02 +0530

    build: register route

--- projects/auth-basics-v2/server.js
+++ projects/auth-basics-v2/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
-const { use } = require("react");
+
 const app = express();
 const port = 3000;
 
@@ -16,19 +16,43 @@ async function main() {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("conneced");
     const userSchema = await mongoose.Schema({
-      email: { type: String, require: true, uniqe: true },
+      email: { type: String, require: true, uniqe: true, lowercase: true },
       passwordHash: { type: String, require: true },
       createdAt: { type: Date, default: Date.now },
     });
 
     const User = await mongoose.model("User", userSchema);
-    const hash = await bcrypt.hash("hello", 10);
-    console.log(hash);
-    const compare = await bcrypt.compare("helo", hash);
-    console.log(compare);
+
+    app.post("/register", async (req, res) => {
+      try {
+        const { email, password } = req.body;
+        const user = await User.find({ email: email });
+
+        if (user.length !== 0) res.status(400).json({ error: "Email Taken" });
+        else {
+          const hash = await bcrypt.hash(password, 10);
+
+          await User.create({ email: email, passwordHash: hash });
+          res.status(201).json({ message: "user Create" });
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    });
+
+    // const compare = await bcrypt.compare("helo", hash);
+    // console.log(compare);
   } catch (err) {
     console.error(err.message);
   }
 }
 
 main();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log("The server run on " + port);
+});
