b7a46c7b07fe673631774b9516e94d9acb8fcc26
Anshuman56 <anshumandehury@yandex.com>
2026-08-24 12:38:39 +0530

    fix: the option paramiter

--- learning/Day78/fetch drill/my-apifetch.js
+++ learning/Day78/fetch drill/my-apifetch.js
@@ -1,5 +1,9 @@
 async function apiFetch(url, path = "", option = {}) {
-  option = { ...{ headers: { "Content-Type": "application/json" } } };
+  option = {
+    ...option,
+    headers: { ...option.headers, "Content-Type": "application/json" },
+  };
+  console.log(option);
   const respons = await fetch(url + path, option);
   if (!respons.ok) {
     const err = new Error("Respons Staturs " + respons.status);
@@ -11,7 +15,17 @@ async function apiFetch(url, path = "", option = {}) {
 }
 async function main() {
   try {
-    const data = await apiFetch("https://note-auth-api.onrender.com", "/notes");
+    const data = await apiFetch(
+      "https://note-auth-api.onrender.com",
+      "/notes",
+      {
+        method: "get",
+        headers: {
+          authorization:
+            "Beareb eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2YTdmZTZkZTdkNDdmZWMyODc0OWQ4NTMiLCJpYXQiOjE3ODc1NTUxNzgsImV4cCI6MTc4NzY0MTU3OH0.7O-IYm6eXIS8aBJ0gYwG8mSmjoavDiiuBQ5zKoQ11eU",
+        },
+      },
+    );
     console.log(data);
   } catch (err) {
     console.log(err.message);
